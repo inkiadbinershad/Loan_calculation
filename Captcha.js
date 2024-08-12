77e41b89c0146129c13bde2e6566d00f93d73444
Inkiad Bin Ershad <136094787+inkiadbinershad@users.noreply.github.com>
2024-08-12 19:29:26 +0600

    Create Captcha.js

---
+++ Captcha.js
@@ -0,0 +1,55 @@
+<div class = "captcha">
+<lable for =  "captcha-input""> enter captcha </lable>
+<div class = "preveiw"></div>
+<div class = "captcha-form">
+ <input type = "text" id = "captcha-form"
+ placeholder = "enter caprcha text">
+ <button class = "captcha-refresh">
+ <i class = "fafa-refressh"></i>
+ </button>
+ </div>
+ 
+ .login-form .captcha label{
+ display :block;
+ front-size:15px;
+ color:#111;
+ margin-bottom:5px;
+ }
+ 
+ .login-form .captch{
+ margin:15px 0px;
+ }
+ 
+ .login-form .captcha .preveiw {
+ color:#555;
+ width:100%;
+ text-align:center;
+ height:40px;
+ line-height:40px;
+ letter-spacing:8px;
+ border:1px sldi#888;
+ font-family:"monospace';
+ }
+ .logoin-form .captcha .preview span{
+ display;inline-block:
+ user-select:none;
+ }
+ .login-form .captcha .captcha-form{
+ display:flex;
+ }
+ .login-form .captcha .captcha-form input{
+ width:100%
+ padding:8px
+ border:1px solid:#888;
+ }
+ .login-form .captcha .captcha-form .captcha-refresh{
+ width:40px;
+ border:none;
+ outline:none;
+ background:#888;
+ colour:#eee;
+ cursor:pointer;
+ }
+ 
+ 
+ 
